f8277fee7074ee129637baa42249fbd3b3e94b1e
edgardanielgd <edgardanielgd123@gmail.com>
2023-05-06 22:56:28 -0500

    Tested generation of reconstructed files

--- main.py
+++ main.py
@@ -17,11 +17,9 @@ print("Read file with custom sampling rate")
 y, sr = librosa.load(
   "car.wav", # File name
   duration=5, # Video time slice (from 0 to duration)
-  sr=22050 # Sampling rate
+  sr=44100, # Sampling rate
+  mono=True, # Convert to mono
 )
-
-print("Printing custom data")
-print(type(y), len(y), type(sr), sr)
 
 print("Quantizing signal")
 
@@ -29,26 +27,31 @@ print("Quantizing signal")
 # to receiver in order to make him able
 # to decode the given output
 
-_, y_levels, parsing_dict = uniform_quantization(
+# Quantize both channels separately
+_, y, parsing_dict = uniform_quantization(
   y, 8
 )
 
 # Get huffman bytes representation
 # for each symbol
-encode_dict, encoded_bits = encode_huffman( y_levels )
+encode_dict, encoded_bits = encode_huffman( y )
 
 # Do not print this, thats not a good idea
 
 ########
 # Now decode data
 decoded_levels = decode_huffman_input( encode_dict, encoded_bits )
-decoded_signal = map(
+
+decoded_signal = np.array(list(map(
   lambda x: parsing_dict[ x ], # Level to signal value
   decoded_levels
-)
+)), dtype=np.float32)
+
+# Test a bit of chaos
+# decoded_signal[44100*2:44100*3] = np.random.rand(44100)
 
 # Create another audio file with the decoded data, same sampling rate
-sf.write("carReconstruct.wav", decoded_signal, 22050 )
+sf.write("carReconstruct.wav", decoded_signal, samplerate = 44100, format="WAV")
 
 """
 f0, voicing, voicing_probability = librosa.pyin(y=y, sr=sr, fmin=50, fmax=300)
